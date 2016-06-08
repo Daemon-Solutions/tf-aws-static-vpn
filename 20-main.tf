07c19f97ff29d0cce14ff39cf9384c0a77f5cfff
Craig Watson <craig@bashton.com>
2016-06-08 12:38:12 +0100

    Silly typo is silly

--- 20-main.tf
+++ 20-main.tf
@@ -15,7 +15,7 @@ resource "aws_customer_gateway" "cgw" {
 }
 
 resource "aws_vpn_connection" "vpn" {
-    vpn_gateway_id      = "${aws_vpn_gateway.vpngw_id}"
+    vpn_gateway_id      = "${aws_vpn_gateway.vpngw.id}"
     customer_gateway_id = "${aws_customer_gateway.cgw.id}"
     type                = "ipsec.1"
     static_routes_only  = true
