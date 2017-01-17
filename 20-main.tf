1f74297a4fd1390e90b634a4dc62b3100a373601
James Hollins <james.hollins@bashton.com>
2017-01-17 15:41:43 +0000

    Added multiple vpn connection functionality, added vars, outputs

--- 20-main.tf
+++ 20-main.tf
@@ -6,26 +6,28 @@ resource "aws_vpn_gateway" "vpngw" {
 }
 
 resource "aws_customer_gateway" "cgw" {
+  count      = "${length(var.cgw_ip)}"
   bgp_asn    = "65000"
-  ip_address = "${var.cgw_ip}"
+  ip_address = "${element(var.cgw_ip, count.index)}"
   type       = "ipsec.1"
   tags {
-    Name = "${var.name}-${var.envtype}-${var.envname}-cgw"
+    Name = "${var.name}-${var.envtype}-${var.envname}-${element(var.vpn_desc, count.index)}-cgw"
   }
 }
 
 resource "aws_vpn_connection" "vpn" {
+    count               = "${length(var.cgw_ip)}"
     vpn_gateway_id      = "${aws_vpn_gateway.vpngw.id}"
-    customer_gateway_id = "${aws_customer_gateway.cgw.id}"
+    customer_gateway_id = "${element(aws_customer_gateway.cgw.*.id, count.index)}"
     type                = "ipsec.1"
     static_routes_only  = true
     tags {
-      Name = "${var.name}-${var.envtype}-${var.envname}-vpn"
+      Name = "${var.name}-${var.envtype}-${var.envname}-${element(var.vpn_desc, count.index)}-vpn"
     }
 }
 
 resource "aws_vpn_connection_route" "routes" {
     count                  = "${length(var.static_routes)}"
     destination_cidr_block = "${element(var.static_routes, count.index)}"
-    vpn_connection_id      = "${aws_vpn_connection.vpn.id}"
+    vpn_connection_id      = "${element(aws_vpn_connection.vpn.*.id, count.index)}"
 }
--- 99-outputs.tf
+++ 99-outputs.tf
@@ -1,9 +1,9 @@
 output "cgw_id" {
-  value = "${aws_customer_gateway.cgw.id}"
+  value = ["${aws_customer_gateway.cgw.*.id}"]
 }
 
 output "vpn_id" {
-  value = "${aws_vpn_connection.vpn.id}"
+  value = ["${aws_vpn_connection.vpn.*.id}"]
 }
 
 output "vgw_id" {
@@ -11,5 +11,5 @@ output "vgw_id" {
 }
 
 output "tunnel_ips" {
-  value = "${aws_vpn_connection.vpn.tunnel1_address},${aws_vpn_connection.vpn.tunnel2_address}"
+  value = ["${aws_vpn_connection.vpn.*.tunnel1_address},${aws_vpn_connection.vpn.*.tunnel2_address}"]
 }
