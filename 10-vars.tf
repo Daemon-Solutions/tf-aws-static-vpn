dbffefba88d67893c50aa58ec51575f62daab1a0
James Hollins <james.hollins@bashton.com>
2017-01-17 10:08:38 +0000

    Removed splits added type list, removed project prefix

--- 10-vars.tf
+++ 10-vars.tf
@@ -1,6 +1,6 @@
 variable "cgw_ip"         {}
 variable "vpc_id"         {}
-variable "static_routes"  {}
-variable "project_prefix" {}
+variable "static_routes"  { type = "list" }
+variable "envname"        {}
 variable "envtype"        {}
 variable "name"           {}
